862086007d275bc0b44ef61c99a049d6712c5407
alexcl01 <acastellanol1@alumnos.nebrija.es>
2024-02-16 16:54:33 +0100

    Tutorial 5

--- DenoFresh/fresh-Tutorial5/routes/buscar.tsx
+++ DenoFresh/fresh-Tutorial5/routes/buscar.tsx
@@ -1,23 +1,40 @@
 import { Handlers, FreshContext, PageProps } from "$fresh/server.ts"
 
+type Character = {
+    name: string;
+    id: string;
+}
+
+type Data = {
+    search?: string;
+    characters: Character[];
+}
+
 export const handler: Handlers = {
     GET: async (req: Request, ctx: FreshContext) => {
         const url = new URL(req.url);
         const search = url.searchParams.get("search");
         if (!search) {
-            return ctx.render([]);
+            return ctx.render({ characters: [], search: "" });
         }
-        return ctx.render([]);
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${search}`,);
+        if (response.status !== 200) {
+            return ctx.render({ characters: [], search });
+        }
+        const data = await response.json();
+        return ctx.render({ characters: data.results, search });
     },
 };
 
-const Page = (props: PageProps) => {
+const Page = (props: PageProps<Data>) => {
     return (
       <>
         <form method="get" action="/buscar">
-            Introduce texto: <input type="text" name="search"/>
-            <button type="Buscar"/>            
+            Introduce texto:{" "} 
+            <input type="text" name="search" defaultValue={props.data.search}/>
+            <button type="submit">Buscar</button>    
         </form>
+        {props.data.characters.map((ch) => <div key={ch.id}>{ch.name}</div>)}
       </>
     );
 };
--- DenoFresh/fresh-Tutorial5/routes/index.tsx
+++ DenoFresh/fresh-Tutorial5/routes/index.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   {
     return 
     <div>
-      
     </div>;
   }
 }
